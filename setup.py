357ee0369cfa3ab2d135d793bed917bab62d31db
Ned Batchelder <ned@edx.org>
2015-01-30 13:58:11 -0500

    Add .gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='edx_lint',
+    version='0.1',
+    description='edX-authored pylint checkers',
+    url='https://github.com/edx/edx-lint',
+    packages=['edx_lint'],
+    install_requires=[
+        'pylint>=1.4.0',
+    ],
+)
