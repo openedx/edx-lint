921add6f64ab547854b5e76a8ba924d4652053bd
Ned Batchelder <ned@edx.org>
2015-02-03 15:22:18 -0500

    Make this ours

--- edx_lint/pylint/__init__.py
+++ edx_lint/pylint/__init__.py
@@ -1,4 +1,11 @@
-"""pylint_django module."""
+"""edx_lint pylint module.
+
+Add this to your pylintrc::
+
+    load-plugins=edx_lint.pylint
+
+"""
+
 from __future__ import absolute_import
 from edx_lint.pylint import plugin
 
