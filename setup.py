05e862690e2da12c66253bb0fc484a6d7fa038ac
Ned Batchelder <ned@edx.org>
2015-02-27 15:04:55 -0500

    An edx_lint_write command.

--- setup.py
+++ setup.py
@@ -7,10 +7,24 @@ setup(
     version='0.1',
     description='edX-authored pylint checkers',
     url='https://github.com/edx/edx-lint',
+
     packages=[
         'edx_lint',
         'edx_lint.pylint',
     ],
+
+    package_data={
+        'edx_lint': [
+            'files/*',
+        ],
+    },
+
+    entry_points={
+        'console_scripts': [
+            'edx_lint_write = edx_lint.write:main',
+        ],
+    },
+
     install_requires=[
         'pylint>=1.4.0',
     ],
