c467c75fdd596f9d0d0b9d42f8491c89cfd08056
Ned Batchelder <ned@edx.org>
2015-03-02 11:28:34 -0500

    Add some minimal help

--- edx_lint/cmd/main.py
+++ edx_lint/cmd/main.py
@@ -11,9 +11,30 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
-    if argv[0] == "check":
+    if not argv or argv[0] == "help":
+        help()
+    elif argv[0] == "check":
         return check_main(argv[1:])
     elif argv[0] == "list":
         return list_main(argv[1:])
     elif argv[0] == "write":
         return write_main(argv[1:])
+    else:
+        print "Don't understand {!r}".format(" ".join(argv))
+        help()
+
+
+def help():
+    print """\
+Manage local config files from masters in edx_lint.
+
+Commands:
+    write FILENAME
+        Write a local copy of FILENAME using FILENAME_tweaks for local tweaks.
+
+    check FILENAME
+        Check that FILENAME has not been edited since writing.
+
+    list
+        List the FILENAMEs that edx_lint can provide.
+"""
