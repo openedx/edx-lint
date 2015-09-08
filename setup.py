3d475f080ca2f6a66df5373189040b7575ee69f9
Jesse Zoldak <zoldak@edx.org>
2015-09-08 11:00:04 -0400

    Bump version to 0.2.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='edx_lint',
-    version='0.2.5',
+    version='0.2.7',
     description='edX-authored pylint checkers',
     url='https://github.com/edx/edx-lint',
 
