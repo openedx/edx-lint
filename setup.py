2fe6b39c71185e2729529d46cf3fc9f914573969
Ned Batchelder <ned@edx.org>
2015-12-20 11:05:32 -0500

    Fix more pylint violations.

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-#!/usr/bin/env python
+"""
+edx-lint
+========
+
+A collection of code quality tools:
+
+- A few pylint plugins to check for quality issues pylint misses.
+
+- A command-line tool to generate config files like pylintrc from a master
+  file (part of edx_lint), and a repo-specific tweaks file.
+
+"""
 
 from setuptools import setup
 
