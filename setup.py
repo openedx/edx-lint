91bbdad0e9367351c830b174a028af674d1d5dbd
Ned Batchelder <ned@edx.org>
2015-12-20 09:01:31 -0500

    Properly label ourselves for PyPI

--- setup.py
+++ setup.py
@@ -11,6 +11,16 @@ setup(
     author_email='oscm@edx.org',
     license='Apache',
 
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
+
     packages=[
         'edx_lint',
         'edx_lint.cmd',
