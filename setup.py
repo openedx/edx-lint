50a72a1b4d3e6517897203c455b2ae22cd21344d
Alexander Zayats <z4y4ts@users.noreply.github.com>
2017-01-25 01:40:13 +0200

    Bump edx-lint version to 0.5.3 and pylint to 1.6.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup
 
 setup(
     name='edx-lint',
-    version='0.5.2',
+    version='0.5.3',
     description='edX-authored pylint checkers',
     url='https://github.com/edx/edx-lint',
     author='edX',
@@ -51,7 +51,7 @@ setup(
     },
 
     install_requires=[
-        'pylint==1.6.4',
+        'pylint==1.6.5',
         'pylint-django>=0.7.2,<1.0.0',
         'pylint-celery==0.3',
         'six>=1.10.0,<2.0.0',
