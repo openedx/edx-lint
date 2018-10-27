daa2f812e9c3fd9a4508c1b619b3912fe099b8b7
Ned Batchelder <ned@edx.org>
2018-10-27 10:54:05 -0400

    Switch to pytest from unittest

--- test/utils.py
+++ test/utils.py
@@ -1,17 +1,7 @@
 """Testing support for pylint plugins."""
 
-import unittest
-
 import astroid.nodes
-from pylint.testutils import CheckerTestCase
 
-
-class PluginUnittestTestCase(CheckerTestCase, unittest.TestCase):
-    def setUp(self):
-        super(PluginUnittestTestCase, self).setUp()
-        # CheckerTestCase is written for pytest, so we need to bridge over to
-        # its setup method.
-        self.setup_method()
 
 def get_module(node):
     """Find the Module node from a child node."""
