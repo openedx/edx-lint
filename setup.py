d7b36342b5511cb6edde0fdd3cf8a1d3c87ba0be
Régis Behmo <regis@behmo.com>
2020-10-14 13:22:41 +0200

    Fix requirements installation
    Running `pip install -r requirements/dev.txt` was failing because `-e .`
    was expanded to an absolute path. This is a long-standing issue in
    pip-tools: https://github.com/jazzband/pip-tools/issues/204
    
    So we got rid of the "-e ." line in dev.in and recompiled the
    requirement files. We realized that this did not pull all requirements
    because some of them were hardcoded in setup.cfg. We decided to move
    those requirements to a base.in file and to dynamically load this file
    from setup.py. This removes the need to manually upgrade a few
    dependencies. To achieve that, we had to add a MANIFEST.in file.
    
    Then, in order to fix tests we had to include test requirements, so we
    created a test.in requirement file.
    
    Note that the `py35-pylint24` still fails because black==20.8b1 is
    incompatible with Python 3.5. AFAIK there is no version compatible with
    Python 3.5.

--- setup.py
+++ setup.py
@@ -13,4 +13,35 @@ A collection of code quality tools:
 
 import setuptools
 
-setuptools.setup()
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+
+    Returns:
+        list: Requirements file relative path strings
+    """
+    requirements = set()
+    for path in requirements_paths:
+        requirements.update(
+            line.split('#')[0].strip() for line in open(path).readlines()
+            if is_requirement(line.strip())
+        )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement.
+
+    Returns:
+        bool: True if the line is not blank, a comment, a URL, or
+              an included file
+    """
+    return line and not line.startswith(("-r", "#", "-e", "git+", "-c"))
+
+
+setuptools.setup(
+    include_package_data=True,
+    install_requires=load_requirements("requirements/base.in"),
+)
