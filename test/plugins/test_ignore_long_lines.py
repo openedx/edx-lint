eb478a1319f77e32a2910334af91e8969570bc0c
Régis Behmo <regis@behmo.com>
2021-02-19 17:33:09 +0100

    fix: unit tests with pylint>=2.6.0
    The upgrade to pylint 2.6.0 broken the unit tests. Turns out that the
    edx_lint.pylint module was no longer being used, so pylint was not reporting
    any lint warning.
    
    Also, for some reason, the `ignore-long-lines` option present in pylintrc is
    ignored in the unit tests, so we need to pass it explicitely to the test
    runner.

--- test/plugins/test_ignore_long_lines.py
+++ test/plugins/test_ignore_long_lines.py
@@ -2,6 +2,7 @@
 Test ignore-long-lines regex
 """
 # pylint: disable=toggle-no-name,toggle-non-boolean-default-value,annotation-missing-token
+import os
 
 from .pylint_test import run_pylint
 
@@ -30,7 +31,8 @@ def test_ignore_long_lines():
         # .. toggle_description: this is a good toggle, it goes back and forth, toggling a lot, which is what we expect toggles to do.
     """
     msg_ids = "line-too-long"
-    messages = run_pylint(source, msg_ids)
+    rcfile = os.path.join(os.path.dirname(__file__), "..", "..", "pylintrc")
+    messages = run_pylint(source, msg_ids, f"--rcfile={rcfile}")
 
     # normalize the messages by stripping off the 3rd segment.
     messages = {":".join(message.split(":")[:2]) for message in messages}
