d082df5800c42ef924162a75b42574c058636947
Ned Batchelder <ned@edx.org>
2022-09-13 12:52:23 -0400

    refactor: make a helper for finding existing edx_lint-written files

--- edx_lint/metadata.py
+++ edx_lint/metadata.py
@@ -3,6 +3,8 @@ Information about the files we can write.
 """
 
 import dataclasses
+import os.path
+
 
 @dataclasses.dataclass
 class Metadata:
@@ -33,3 +35,12 @@ KNOWN_FILES = {
     # BTW: common_constraints.txt is in the resources, but isn't a file
     # that edx_lint writes.
 }
+
+
+def existing_known_files():
+    """Yield the names of files that edx_lint writes, and that exist."""
+    for filename, metadata in KNOWN_FILES.items():
+        if metadata.internal_only:
+            continue
+        if os.path.exists(filename):
+            yield filename
