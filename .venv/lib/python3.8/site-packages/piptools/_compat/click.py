dfa9cb0a1856ba0b3f972921c6e0a3dad88f1d39
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2022-11-30 16:15:09 +0500

    fix: ylint<2.15.0 check removed

---
+++ .venv/lib/python3.8/site-packages/piptools/_compat/click.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import click
+
+CLICK_MAJOR_VERSION = int(
+    # extract major version of click
+    click.__version__.split(".")[0]
+)
+IS_CLICK_VER_8_PLUS = CLICK_MAJOR_VERSION > 7
---
+++ .venv/lib/python3.8/site-packages/piptools/locations.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from pip._internal.utils.appdirs import user_cache_dir
+
+# The user_cache_dir helper comes straight from pip itself
+CACHE_DIR = user_cache_dir("pip-tools")
